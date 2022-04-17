06ca9c4613db646ea2eb7cd30840277a54eb5fe0
Flavio Melo <fhvmelo@gmail.com>
2022-04-17 14:51:51 -0300

    13 - Trabalhando com módulos em JavaScript
    - Utilizando módulos;
    - Atividade prática (importar e exportar funções através de módulos em .mjs).

---
+++ 13 - Trabalhando com módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Flávio", 34));
+console.log(mostraCidade("Flávio", "Fortaleza"));
+console.log(mostraHobby("Flávio", "Estudar idiomas"));
